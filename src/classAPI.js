3b9f4f84bd42b37b9c8100ff6048f965ef46e52a
Vern Hockney <vern@laut.fm>
2019-03-22 12:45:41 +0100

    add documentation

--- src/classAPI.js
+++ src/classAPI.js
@@ -1,26 +1,52 @@
 import axios from 'axios'
 
+/** Class holding the base configuration for all instantiated API Classes */
 class AdminApiConfig {
+
+    /**
+     * Create the basic configuration requirments
+     */
     constructor () {
         this.defaultToken = null
         this.defaultOrigin = null
         this.baseURL = 'https://api.radioadmin.laut.fm/stations/'
     }
 
+    /**
+     * Set a user provided token
+     * @param {string} token - the user provided token
+     */
     set token ( token ) {
         this.defaultToken = token
         this._token( token )
     }
 
+    /**
+     * Set a user provided origin
+     * @param {string} origin - the user provided origin
+     */
     set origin ( origin ) {
         this.defaultOrigin = origin
         this._origin( origin )
     }
 
+    /**
+     * Set the base url to be used for all API calls
+     * @param {string} url - the base url
+     */
     set baseUrl ( url ) {
         this.api.defaults.baseURL = url
     }
 
+    /**
+     * Checks if a user token is provided and if none is provided
+     * it sets the http header token to the one stored in the window object.
+     * If a token is provided, it sets the http header token as
+     * provided by the user
+     * @protected
+     * @param {string = null} token - the token which was set by the user
+     * @returns {object} this
+     */
     _token ( token = null ) {
         if ( !token ) {
             token = window.localStorage.getItem( 'laut.fm.api.token' )
@@ -29,6 +55,12 @@ class AdminApiConfig {
         return this
     }
 
+    /**
+     * Sets the http origin header with that origin provided by the user.
+     * @private
+     * @param {string} origin - the user which was set by the user
+     * @returns {object} this
+     */
     _origin ( origin ) {
         let headers = this.api.defaults.headers.common[ 'Origin' ] = origin
         return this
@@ -36,7 +68,15 @@ class AdminApiConfig {
 
 }
 
+/**
+ * Class handeling all the basic logic of API calls and response preparation
+ * @extends AdminApiConfig
+ */
 class AdminApiClass extends AdminApiConfig {
+    /**
+     * Creates the http request instance
+     * @ returns {object} this
+     */
     constructor () {
         super()
         this.data = {}
@@ -48,7 +88,97 @@ class AdminApiClass extends AdminApiConfig {
         return this
     }
 
-    path ( path ) {
+    /**
+     * Initiates the http request with a get request
+     * @param {string = ''} path - the path to be appended
+     * to the base url
+     * @returns {Promise} the promise of the get request
+     */
+    get ( path = '' ) {
+        this._resetData()
+        return this.api.get( this._path(path) )
+    }
+
+    /**
+     * Initiates the http request with a post request
+     * @param {string = ''} path - the path to be appended
+     * to the base url
+     * @returns {Promise} the promise of the post request
+     */
+    post ( path = '' ) {
+        return this.api.post( this._path(path), this.data )
+    }
+
+    /**
+     * Initiates the http request with a patch request
+     * @param {string = ''} path - the path to be appended to the base url
+     * @returns {Promise} the promise of the patch request
+     */
+    patch ( path = '' ) {
+        return this.api.patch( this._path(path), this.data )
+    }
+
+    /**
+     * Initiates the http request with a put request
+     * @param {string = ''} path - the path to be appened to the base url
+     * @returns {Promise} the promise of the put request
+     */
+    put ( path = '' ) {
+        return this.api.put( this._path(path), this.data )
+    }
+
+    /**
+     * Initiates the http request with an options request
+     * @param {string = ''} path - the path to be appended to the base url
+     * @returns {Promise} the promise of the options request
+     */
+    options ( path = '' ) {
+        this._resetData()
+        return this.api.options( this._path(path) )
+    }
+
+    /**
+     * Initiates the http request with a delete request
+     * @param {string = ''} path - the path to be appended to the base url
+     * @returns {Promise} the promise of the delete request
+     */
+    delete ( path = '' ) {
+        this._resetData()
+        return this.api.delete( this._path(path) )
+    }
+
+    /**
+     * Provides the station ID to be used in the path function.
+     * This allowes the base url to remain unchanged and therefore
+     * provides the opportunity for multiple stations to be queried with
+     * only one base class
+     * @param {number} stationId - the station ID to be queried
+     * @returns {object} this
+     */
+    station ( stationId ) {
+        this.apiStation = stationId
+        return this
+    }
+
+    /**
+     * Provides the data to send with the request
+     * @param {object} data - the data to send with the request
+     * @returns {object} this
+     */
+    with ( data ) {
+        this.data = data
+        return this
+    }
+
+    /**
+     * Standardizes the building of the path for
+     * the api call
+     * @private
+     * @param {string} path - the path path provided by
+     * the user in the verb
+     * @returns {string} the build path
+     */
+    _path ( path ) {
         let builtPath = ''
         if (this.apiStation) {
            builtPath += `${this.apiStation}` 
@@ -61,43 +191,11 @@ class AdminApiClass extends AdminApiConfig {
         return builtPath 
     }
 
-    get ( path = '' ) {
-        this._resetData()
-        return this.api.get( this.path(path) )
-    }
-
-    post ( path ) {
-        return this.api.post( this.path(path), this.data )
-    }
-
-    patch ( path ) {
-        return this.api.patch( this.path(path), this.data )
-    }
-
-    put ( path ) {
-        return this.api.put( this.path(path), this.data )
-    }
-
-    options ( path ) {
-        this._resetData()
-        return this.api.options( this.path(path) )
-    }
-
-    station ( stationId ) {
-        this.apiStation = stationId
-        return this
-    }
-
-    delete ( path ) {
-        this._resetData()
-        return this.api.delete( this.path(path) )
-    }
-
-    with ( data ) {
-        this.data = data
-        return this
-    }
-
+    /**
+     * Resets the data object initialized in the constructor after a post
+     * or put request has been made
+     * @private
+     */
     _resetData () {
         this.data = {}
     }
